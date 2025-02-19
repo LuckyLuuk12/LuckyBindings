4022f2af64ad74f64f179c6b1598e0616b9e8ac2
Luuk Kablan <luckyluuk02@gmail.com>
2025-02-19 13:09:16 +0100

    feat: added docs and runRepeatedly (async) method

--- src/main/java/me/luckyluuk/luckybindings/handlers/Scheduler.java
+++ src/main/java/me/luckyluuk/luckybindings/handlers/Scheduler.java
@@ -8,7 +8,22 @@ public class Scheduler {
   private static final int availableProcessors = Runtime.getRuntime().availableProcessors();
   private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(availableProcessors);
 
+  /**
+   * Run a task after a delay.
+   * @param task The task to run
+   * @param delay The delay in milliseconds before the task is run
+   */
   public static void runLater(Runnable task, long delay) {
     scheduler.schedule(task, delay, TimeUnit.MILLISECONDS);
+  }
+
+  /**
+   * Run a task repeatedly with a fixed delay and an optional initial delay.
+   * @param task The task to run
+   * @param period The delay between each run in milliseconds
+   * @param initialDelay The optional initial delay in milliseconds before the first run, default is 20 milliseconds
+   */
+  public static void runRepeatedly(Runnable task, long period, long... initialDelay) {
+    scheduler.scheduleAtFixedRate(task, initialDelay.length > 0 ? initialDelay[0] : 20, period, TimeUnit.MILLISECONDS);
   }
 }
