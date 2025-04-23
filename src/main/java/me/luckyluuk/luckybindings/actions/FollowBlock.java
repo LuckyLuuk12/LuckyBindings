4fdda7472825f1eece2748dbad12f0ff1cf98c6d
Luuk Kablan <luckyluuk02@gmail.com>
2025-04-23 15:23:05 +0200

    feat: better and more organized arguments for the FollowBlock action

--- src/main/java/me/luckyluuk/luckybindings/actions/FollowBlock.java
+++ src/main/java/me/luckyluuk/luckybindings/actions/FollowBlock.java
@@ -30,12 +30,13 @@ public class FollowBlock extends Action {
         Using this action will cause the player to start following the specified block.
 
         PARAMETERS:
-        - The block to follow (required).
-        - Whether to sprint while following the block (optional, default is false).
-        - The maximum search distance (optional, default is 3).
-        - The maximum gap size allowed in the path (optional, default is 0).
-        - The distance your player must have from other players before stopping (optional, default is 32).
+        1. The block to follow (required).
+        2. Whether to sprint while following the block (optional, default is false).
+        3. The maximum search distance (optional, default is 64).
+        4. The amount of path-sharpening rounds (optional, default is 2).
+        5. The distance your player must have from other players before stopping (optional, default is 32).
            This is to prevent detection by staff or other players.
+        6. Whether to display the path with particles values <= 0 mean false (optional, default is false).
         """);
     setArgs(args);
   }
@@ -44,38 +45,40 @@ public class FollowBlock extends Action {
   public void execute() {
     ClientPlayerEntity player = MinecraftClient.getInstance().player;
     if (player == null || block == null) return;
-
     isActivated = !isActivated;
     if (!isActivated) {
       path.clear();
       return;
     }
-
+    // Find the most optimal path to walk the shape of the pathway consisting of the target block
     PathMiddleFinder pmf = new PathMiddleFinder(player, block, options);
     path = new LinkedList<>(pmf.findPath());
     Queue<BlockPos> runningPath = new LinkedList<>(path);
+    // If the path is empty, stop the action and notify the player
     if (path.isEmpty()) {
       player.sendMessage(Text.literal("No path found!"), false);
       isActivated = false;
       return;
     }
     player.sendMessage(Text.literal("Path found with " + path.size() + " blocks."), false);
+    // Debug / Display the path
     Scheduler.runRepeatedly(task -> {
       if(!isActivated) {
         task.cancel(true);
         return;
       }
-      if(path == null || path.isEmpty()) return;
+      if(path == null || path.isEmpty() || options.size() > 5 && options.stream().skip(5).findFirst().orElse(0) <= 0) {
+        return;
+      }
       pmf.debugPath(path.stream().toList());
     }, 2L, 0L);
-
-      // Start walking through the cycle TODO: Make the movement "smoother", for some reason there is a large interval per step in the path
-//    CompletableFuture.runAsync(() -> {
-//      while (isActivated && !player.isRemoved() && player.isLoaded()) {
-//        walkPath(new LinkedList<>(runningPath), player).join();
-//      }
-//      player.velocityModified = false;
-//    });
+    // Start walking through the cycle TODO: Make the movement "smoother", for some reason there is a large interval per step in the path
+    CompletableFuture.runAsync(() -> {
+      while (isActivated && !player.isRemoved() && player.isLoaded()) {
+        walkPath(new LinkedList<>(runningPath), player).join();
+      }
+      player.velocityModified = false;
+    });
   }
 
   @Override
@@ -84,9 +87,6 @@ public class FollowBlock extends Action {
       this.block = Registries.BLOCK.get(Identifier.of(args[0]));
     }
     this.sprint = args.length > 1 && Boolean.parseBoolean(args[1]);
-//    if (args.length > 2) this.maxSearchDistance = Integer.parseInt(args[2]);
-//    if (args.length > 3) this.maxGaps = Integer.parseInt(args[3]);
-//    if (args.length > 4) this.stopWhenNearbyDistance = Integer.parseInt(args[4]);
     for (int i = 2; i < args.length; i++) options.add(Integer.parseInt(args[i]));
   }
 
