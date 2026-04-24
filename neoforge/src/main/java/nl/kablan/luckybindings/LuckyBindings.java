93414b0b15a5cd0d150248ebaf17966861644ae5
Luuk Kablan <luckyluuk02@gmail.com>
2026-04-24 22:46:16 +0200

    feat: neoforge tested and seems to work

--- neoforge/src/main/java/nl/kablan/luckybindings/LuckyBindings.java
+++ neoforge/src/main/java/nl/kablan/luckybindings/LuckyBindings.java
@@ -2,10 +2,14 @@ package nl.kablan.luckybindings;
 
 
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 
 import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.common.NeoForge;
 import nl.kablan.luckybindings.commands.LuckyBindingsCommands;
+import nl.kablan.luckybindings.platform.Services;
 
 @Mod(Constants.MOD_ID)
 public class LuckyBindings {
@@ -21,7 +25,13 @@ public class LuckyBindings {
         CommonClass.init();
 
         // NeoForge does not have ModMenu; expose config via client command.
-        eventBus.addListener(this::onRegisterClientCommands);
+        NeoForge.EVENT_BUS.addListener(this::onRegisterClientCommands);
+
+        // It has a kinda built-in ModMenu tho for which we can just register our config screen:
+        ModLoadingContext.get().registerExtensionPoint(
+          IConfigScreenFactory.class,
+          () -> (minecraft, parent) -> Services.CONFIG_SCREEN.createConfigScreen(parent)
+        );
     }
 
     private void onRegisterClientCommands(RegisterClientCommandsEvent event) {
